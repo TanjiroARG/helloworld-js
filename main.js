8be5f54d6ee1a10611bccd96c88ff20243e497a2
Aliencito | Tanjiro <123867904+TanjiroARG@users.noreply.github.com>
2023-01-29 16:39:31 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("\n\n\n\n\n\n\n\n");
+console.log("#####################################");
+console.log("#                                   #");
+console.log("#        Hola, mundo!               #");
+console.log("#                                   #");
+console.log("#####################################\n\n\n\n\n\n\n\n");
